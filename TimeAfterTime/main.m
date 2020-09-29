4f21a573c098b5dbb08846ef08a16c6e4da5feaa
Nick <ptnguyen1901@gmail.com>
2020-09-29 08:25:33 -0400

    test

--- TimeAfterTime/main.m
+++ TimeAfterTime/main.m
@@ -16,6 +16,22 @@ int main(int argc, const char * argv[]) {
         double seconds = [now timeIntervalSince1970];
         NSLog(@"It has been %f seconds since the start of 1970.",seconds);
 
+
+        NSDate *later = [now dateByAddingTimeInterval:100000];
+        NSLog(@"In 100,000 seconds it will be %@",later);
+
+        NSCalendar *calendar = [NSCalendar currentCalendar];
+        NSLog(@"My calendar is %@",[calendar calendarIdentifier]);
+
+        unsigned long day = [calendar ordinalityOfUnit:NSCalendarUnitDay
+                                                inUnit:NSCalendarUnitMonth
+                                               forDate:now];
+
+        NSLog(@"This is day %lu of the month",day);
+
+
+        [calendar ordinalityOfUnit:NSCalendarUnitDay inUnit:NSCalendarUnitMonth forDate:now];
+
         NSHost *currentHost = [[NSHost alloc]init];
         NSString *name = [currentHost localizedName];
         NSLog(@"My computer name is %@",name);
