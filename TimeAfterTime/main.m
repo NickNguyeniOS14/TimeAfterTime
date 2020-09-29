3fdcd83e2b2789aadd9a9314ead5a8c124e4571e
Nick <ptnguyen1901@gmail.com>
2020-09-29 08:07:23 -0400

    1970

--- TimeAfterTime/main.m
+++ TimeAfterTime/main.m
@@ -12,6 +12,9 @@ int main(int argc, const char * argv[]) {
         NSDate *now = [NSDate date];
         NSLog(@"This NSDate object lives at %p", now);
         NSLog(@"The date is %@", now);
+
+        double seconds = [now timeIntervalSince1970];
+        NSLog(@"It has been %f seconds since the start of 1970.",seconds);
     }
     return 0;
 }
