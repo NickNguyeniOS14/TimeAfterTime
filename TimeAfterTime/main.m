911017b84da7eef99f2239328e47f9333d66ad3d
Nick <ptnguyen1901@gmail.com>
2020-09-29 07:58:39 -0400

    Initial Commit

---
+++ TimeAfterTime/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  TimeAfterTime
+//
+//  Created by Nick Nguyen on 9/29/20.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
